266f9ae91fa4d078d7e4586a4268c4459bde39b6
Amanda Murphy <murphdizle@Hotmail.com>
2015-02-27 12:45:01 -0800

    Update App.java

--- App.java
+++ App.java
@@ -27,10 +27,42 @@ public class App {
 		list.add(trans);
 		accountBalance += ((Addition) trans).add(amount);
 	}
+	public void add(){
+		Float amount = (float) 0;
+		System.out.println("Please enter the amount you wish to deposit");
+		while(!in.hasNextFloat())
+		{
+			System.out.println("Error: Please enter an amount.");		
+			in.nextLine();
+		}
+		amount = in.nextFloat();
+		in.nextLine();
+		
+		System.out.println("Please enter the description of the transaction");
+		String description = in.nextLine();
+		transaction trans = new Addition(accountNumber, description, amount);
+		list.add(trans);
+		accountBalance += ((Addition) trans).add(amount);
+	}
 	public void subtract(double amount, String description){
 		transaction trans = new Subtraction(description, (amount * -1));
 		list.add(trans);
 		accountBalance -= ((Subtraction) trans).subtract(amount);
+	}
+		public void subtract(){
+		System.out.println("Please enter the amount you wish to deposit");
+		while(!in.hasNextInt())
+		{
+			System.out.println("Error: Please enter an amount. ");
+			in.nextLine();
+		}
+		int debit = in.nextInt();
+		in.nextLine();
+		System.out.println("Please enter the description of the transaction");
+		String comment = in.nextLine();
+		transaction trans = new Subtraction(accountNumber, comment, (debit * -1));
+		list.add(trans);
+		accountBalance -= ((Subtraction) trans).subtract(debit);
 	}
 	public double getBalance(){
 		return accountBalance;
@@ -42,12 +74,24 @@ public class App {
 		list.search(string);
 		System.out.println();
 	}
+		public void search(){
+		System.out.println("what do you want to search for?");
+		in.nextLine();
+		String string = in.nextLine();
+		System.out.println("Searching for \"" + string + "\"");
+		System.out.printf("%-15s %-20s %-15s %n", "Transaction", "Description", "Debit/Credit");
+		System.out.println("*************************************************");
+		list.search(string);
+		System.out.println();
+	}
 	public void remove(String string){
 		list.remove(string);
 	}
-	public void remove(int index){
-		System.out.println("Removing the " + (index + 1) + "th item");
-		list.remove(index);
+	public void remove(){
+		in.nextLine();
+		System.out.println("Please enter the description of what you want removed");
+		String remove = in.nextLine();
+		list.remove(remove);
 	}
 	public void print(){
 		System.out.println("_________________________________________________");
@@ -70,42 +114,30 @@ public class App {
 		System.out.println("(Q) to quit");
 		System.out.println();
 	}
-	public void options(String ch){
+		public void options(String ch){
 		switch(ch.toLowerCase()){
 		 case "d":
 			 print();
 			 break;
 		 case "a":
-			 System.out.println("Please enter the amount you wish to deposit");
-			 int amount = in.nextInt();
-			 in.nextLine();
-			 System.out.println("Please enter the description of the transaction");
-			 String description = in.nextLine();
-			 add(amount, description);
+			 add();
 			 break;
 		 case "s":
-			 System.out.println("what do you want to search for?");
-			 in.nextLine();
-			 String string = in.nextLine();
-			 search(string);
+			 search();
 			 break;
 		 case "b":
-			 System.out.println("Please enter the amount you wish to deposit");
-			 int debit = in.nextInt();
-			 in.nextLine();
-			 System.out.println("Please enter the description of the transaction");
-			 String comment = in.nextLine();
-			 subtract(debit, comment);
+			 subtract();
 			 break;
 		 case "r":
-			 System.out.println("Please enter the description of what you want removed");
-			 in.nextLine();
-			 String remove = in.nextLine();
-			 remove(remove);
+			 remove();
 			 break;
 		 case "q":
 			 System.out.println("Thank you! Have a nice day!");
 			 System.exit(0);
+		default:
+			System.out.println("Illegal Command!");
+			break;
 		}
+
 	}
 }
